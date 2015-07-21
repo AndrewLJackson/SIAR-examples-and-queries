488983588c09cad8505a376b4e47518780a6a752
Andrew Jackson <a.jackson@tcd.ie>
2015-07-21 21:34:50 +0100

    finished 1st draft of community metrics Rmd
    also changed variable names in demo.bayesian.layman.r

--- siber-scripts/demo.bayesian.layman.r
+++ siber-scripts/demo.bayesian.layman.r
@@ -19,9 +19,9 @@ mydata <- read.csv("example_layman_data_2.csv", header=T)
 attach(mydata) # make the names of the columns available for direct calling
 
 # calculate the Bayesian Layman metrics given data for Isotopes 1 and 2, 
-# a grouping variable Group and a number of iterations to use to generate
+# a grouping variable group and a number of iterations to use to generate
 # the results
-metrics <- siber.hull.metrics(Iso1,Iso2,Group,R=10^4)
+metrics <- siber.hull.metrics(x,y,group,R=10^4)
 
 
 
@@ -32,37 +32,49 @@ metrics <- siber.hull.metrics(Iso1,Iso2,Group,R=10^4)
 
 # Plot the raw [simulated in this case] data
 
+# these are the names of each of the metrics taken from the fitted model
 xlabels <- attributes(metrics)$dimnames[[2]]
 
 # Now lets calculate the convex hull as per the current method based
 # simply on the means for each group
-means.x <- aggregate(Iso1,list(Group),mean)$x
-means.y <- aggregate(Iso2,list(Group),mean)$x
+means.x <- aggregate(x,list(group),mean)$x
+means.y <- aggregate(y,list(group),mean)$x
 sample.hull <- convexhull(means.x,means.y)
 
-M <- max(Group)
+# knowing how many groups we have is useful for constraining the plot 
+M <- max(group)
 
 #dev.new()
 par(mfrow=c(1,1))
-plot(Iso1,Iso2,col=Group,
-     xlab="Isotope 1",
-     ylab="Isotope 2",
-  pch=1,asp=1,xlim=c(min(Iso1)-2,max(Iso1)+2),ylim=c(min(Iso2)-2,max(Iso2)+2))
-lines(sample.hull$xcoords,sample.hull$ycoords,lty=1,col=1,lwd=2)
+plot(x, y,
+     col = group,
+     xlab = "Isotope 1",
+     ylab = "Isotope 2",
+     pch = 1, asp=1, 
+     xlim = c( min(x)-2, max(x)+2), 
+     ylim = c( min(y)-2, max(y)+2)
+     )
+
+lines(sample.hull$xcoords, sample.hull$ycoords, lty = 1, col = 1, lwd = 2)
+
 legend("topleft",
-  legend=as.character(c(paste("Group ",1:M),"sample hull")),
-  pch=c(rep(1,M),NA),col=c(1:M,1,1), lty=c(rep(NA,M),1),
-  bty="n")
+  legend = as.character(c(paste("group ",1:M),"sample hull")),
+  pch = c(rep(1,M),NA), col = c(1:M,1,1), lty = c(rep(NA,M),1),
+  bty = "n")
 
 # in this example, I plot TA as a histogram seperately to the other
 # metrics as it is usually on a scale so vastly different from the other 
 # metrics.
 #dev.new()
-par(mfrow=c(1,2))
-hist(metrics[,"TA"],freq=F,xlab="TA",ylab="Density",main="")
+par(mfrow = c(1,2))
+
+hist(metrics[,"TA"], freq = F, xlab = "TA", ylab = "Density", main="")
+
 siardensityplot(metrics[,c(1,2,4,5,6)],
-      xticklabels=xlabels[c(1,2,4,5,6)],
-      ylims=c(0,25),ylab="units",xlab="Metric")
+                xticklabels = xlabels[c(1,2,4,5,6)],
+                ylims = c(0,25),
+                ylab = expression('\u2030'),
+                xlab = "Metric")
 
 
 detach(mydata)
